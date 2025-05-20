fce2d1b0f7a2c6cc99c5875d1cad56fcba833217
Gustavo M Silva <silvamacedo.gustavo2004@gmail.com>
2025-05-19 21:16:02 -0300

    hãn oruan né

--- Testes Cypress/cypress/e2e/ExemploAula2.cy.js
+++ Testes Cypress/cypress/e2e/ExemploAula2.cy.js
@@ -1,7 +1,7 @@
 /// <reference = cypress>
 
 describe("Testes da criação, registro e login", ()=>{
-    it.skip("Teste de criação de usuario com sucesso", ()=>{
+    it("Teste de criação de usuario com sucesso", ()=>{
         cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
         cy.get('.btn-link').click()
         cy.get('#firstName').type("Guga")
@@ -11,7 +11,7 @@ describe("Testes da criação, registro e login", ()=>{
         cy.get('.btn-primary').click()
         cy.get('.ng-binding').should("contain.text", "Registration successful")
     })
-    it.skip("Teste de criação de usuario com falha", ()=>{
+    it("Teste de criação de usuario com falha", ()=>{
         cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
         cy.get('.btn-link').click()
         cy.get('#firstName').type("Guga")
@@ -19,7 +19,7 @@ describe("Testes da criação, registro e login", ()=>{
         cy.get('#username').type("Guga")
         cy.get('.btn-primary').should("be.disabled")
     })
-    it.skip("Teste de Login com sucesso", ()=>{
+    it("Teste de Login com sucesso", ()=>{
         let infos = criarUser()
         cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
         cy.get('#username').type(infos[0])
